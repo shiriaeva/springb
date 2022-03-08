ac9d2e551dd0039fb41e9b87023c0f3dfea5b3e2
shiriaeva <Elena.Shiriaeva@urfu.me>
2022-03-09 01:19:44 +0500

    first commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setFirstName("Юрий");
+        person.setSecondName("Рыбников");
+
+        System.out.println(person.getFirstName() + " " + person.getSecondName());
+    }
+
+}
